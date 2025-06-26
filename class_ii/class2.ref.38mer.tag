10c55ab0ff32a6cdb090ccd2c229150d904af340
simo <svm.zhang@gmail.com>
2025-06-25 23:19:39 -0700

    initial commit: class II reference files

---
+++ class_ii/class2.ref.38mer.tag
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f908a4ea8e9155ffb3c2ccdf0d951612d8e8f0de66b3e5bb27ddb0142aa5a95
+size 9054513
---
+++ class_ii/class2.ref.nix
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4743ef23cb52a485bd34e1fc23fbaf40751f3def0612556136f3d54494a63828
+size 60241496
